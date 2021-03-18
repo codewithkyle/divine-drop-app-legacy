cb75c8e86624ae182c5803ebbb5ea61128071a69
Kyle Andrews <codingwithkyle@gmail.com>
2021-03-18 13:46:39 -0400

    Hotfixing new build tools

--- build/injector.js
+++ build/injector.js
@@ -1,17 +1,14 @@
 const fs = require("fs");
 const path = require("path");
 const cwd = process.cwd();
-require("dotenv").config();
-
-const API_URL = process.env?.DEV_API_URL ?? "https://api.divinedrop.app";
 
 const AppSettingsFile = path.join(cwd, "Client", "Models", "Globals", "AppSettings.cs");
 const ConfigFile = path.join(cwd, "Client", "Scripts", "config.ts");
 
 let data = fs.readFileSync(AppSettingsFile, { encoding: "utf-8"});
-data = data.replace(/public\sstatic\sstring\sAPI \=.*?\;/, `public static string API = "${API_URL}";`);
+data = data.replace(/public\sstatic\sstring\sAPI \=.*?\;/, `public static string API = "${process.env.API_URL}";`);
 fs.writeFileSync(AppSettingsFile, data);
 
 data = fs.readFileSync(ConfigFile, { encoding: "utf-8"});
-data = data.replace(/const\sAPI\_URL\s\=.*?\;/, `const API_URL = "${API_URL}";`);
+data = data.replace(/const\sAPI\_URL\s\=.*?\;/, `const API_URL = "${process.env.API_URL}";`);
 fs.writeFileSync(ConfigFile, data);
