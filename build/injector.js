b779536fcdb3f040b67b845b2c80588030c27ea4
Kyle Andrews <codingwithkyle@gmail.com>
2021-03-18 13:54:16 -0400

    Hotfixing new build tools

--- build/injector.js
+++ build/injector.js
@@ -1,14 +1,17 @@
 const fs = require("fs");
 const path = require("path");
 const cwd = process.cwd();
+require("dotenv").config();
+
+console.log(process.env.API_URL);
 
 const AppSettingsFile = path.join(cwd, "Client", "Models", "Globals", "AppSettings.cs");
 const ConfigFile = path.join(cwd, "Client", "Scripts", "config.ts");
 
-let data = fs.readFileSync(AppSettingsFile, { encoding: "utf-8"});
+let data = fs.readFileSync(AppSettingsFile, { encoding: "utf-8"}).toString();
 data = data.replace(/public\sstatic\sstring\sAPI \=.*?\;/, `public static string API = "${process.env.API_URL}";`);
 fs.writeFileSync(AppSettingsFile, data);
 
-data = fs.readFileSync(ConfigFile, { encoding: "utf-8"});
+data = fs.readFileSync(ConfigFile, { encoding: "utf-8"}).toString();
 data = data.replace(/const\sAPI\_URL\s\=.*?\;/, `const API_URL = "${process.env.API_URL}";`);
 fs.writeFileSync(ConfigFile, data);
